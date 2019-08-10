0e46a46b5efd14e5b7e7ed068dd50dd808a7df1b
MotilalChavhan <motilalvijaychavhan@gmail.com>
2019-08-10 17:05:32 +0530

    Updates

---
+++ precache-manifest.3d706a37106699add4c947e7a19eb35c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "621975d094c7f633675670fe48bb9d59",
+    "url": "/RoboFriends/index.html"
+  },
+  {
+    "revision": "f7fbec1799e2317df57d",
+    "url": "/RoboFriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "f5bf4dc24f4ae3bc2a67",
+    "url": "/RoboFriends/static/css/main.de32d735.chunk.css"
+  },
+  {
+    "revision": "f7fbec1799e2317df57d",
+    "url": "/RoboFriends/static/js/2.e517eafc.chunk.js"
+  },
+  {
+    "revision": "f5bf4dc24f4ae3bc2a67",
+    "url": "/RoboFriends/static/js/main.4e7d95ce.chunk.js"
+  },
+  {
+    "revision": "6e193c04264faf375a53",
+    "url": "/RoboFriends/static/js/runtime~main.140252ec.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
