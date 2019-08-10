21bf6ab5600cf6c78c21dc616795eae1ca40c13c
MotilalChavhan <motilalvijaychavhan@gmail.com>
2019-08-10 16:08:51 +0530

    deploying the app on github pages

---
+++ build/precache-manifest.9ad35565a316b9944899d418c748c1e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "643ad6294f5d3940a6155e8152cf2b5a",
+    "url": "/RoboFriends/index.html"
+  },
+  {
+    "revision": "f7fbec1799e2317df57d",
+    "url": "/RoboFriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "85c79a7fa41acad5050e",
+    "url": "/RoboFriends/static/css/main.de32d735.chunk.css"
+  },
+  {
+    "revision": "f7fbec1799e2317df57d",
+    "url": "/RoboFriends/static/js/2.e517eafc.chunk.js"
+  },
+  {
+    "revision": "85c79a7fa41acad5050e",
+    "url": "/RoboFriends/static/js/main.ce0bd91a.chunk.js"
+  },
+  {
+    "revision": "6e193c04264faf375a53",
+    "url": "/RoboFriends/static/js/runtime~main.140252ec.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
